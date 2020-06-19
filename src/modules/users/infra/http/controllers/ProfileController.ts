5b079b4449602e6872384b5754f075d68ecd83d8
Adriano Luis de Almeida <adrianoo.luis.almeida@gmail.com>
2020-06-18 23:43:06 -0300

    class transform

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
+import { classToClass } from 'class-transformer';
 
 export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
@@ -13,7 +14,7 @@ export default class ProfileController {
 
     delete user.password;
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -30,8 +31,6 @@ export default class ProfileController {
       password,
     });
 
-    delete user.password;
-
-    return response.send(user);
+    return response.json(classToClass(user));
   }
 }
