448620e2021d90aecc04675cc1dfcf4ca7a8cae6
Adriano Luis de Almeida <adrianoo.luis.almeida@gmail.com>
2020-06-21 20:02:34 -0300

    send mail with amazon ses

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'adriano@economei.com.br',
+      name: 'Adriano Almeida',
+    },
+  },
+} as IMailConfig;
